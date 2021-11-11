799f7e22bbd78c7d87df5f6284a8041d5f8433c5
Oluwayemisi Runsewe <runseweoluwayemisi@gmail.com>
2021-11-10 20:38:22 -0600

    Connect to DB and make endpoints

---
+++ routes/resource.js
@@ -0,0 +1,31 @@
+var express = require('express'); 
+var router = express.Router(); 
+ 
+// Require controller modules. 
+var api_controller = require('../controllers/api'); 
+var televisionShow_controller = require('../controllers/televisionShow'); 
+ 
+/// API ROUTE /// 
+ 
+// GET resources base. 
+router.get('/', api_controller.api); 
+ 
+/// televisionShow ROUTES /// 
+ 
+// POST request for creating a televisionShow.  
+router.post('/resource/televisionShows', televisionShow_controller.televisionShow_create_post); 
+ 
+// DELETE request to delete televisionShow. 
+router.delete('/resource/televisionShows/:id', televisionShow_controller.televisionShow_delete); 
+ 
+// PUT request to update televisionShow. 
+router.put('/resource/televisionShows/:id', 
+televisionShow_controller.televisionShow_update_put); 
+ 
+// GET request for one televisionShow. 
+router.get('/resource/televisionShows/:id', televisionShow_controller.televisionShow_detail); 
+ 
+// GET request for list of all televisionShow items. 
+router.get('/televisionShows', televisionShow_controller.televisionShow_list); 
+ 
+module.exports = router; 
--- routes/televisionShows.js
+++ routes/televisionShows.js
@@ -1,9 +1,8 @@
 var express = require('express');
+const televisionShow_controller = require('../controllers/televisionShow');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('televisionShows', { title: 'Search Results' });
-});
+router.get('/', televisionShow_controller.televisionShow_view_all_Page);
 
 module.exports = router;
